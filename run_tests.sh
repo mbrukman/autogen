6a9858186cd50386ede053f9c58ad4ecf401a8bb
Misha Brukman <mbrukman@google.com>
2015-10-24 23:48:20 -0500

    Refactored run_all_tests() function.
    Added a run_one_test() method to enable code sharing with future test runners.
    Also, the script now returns a zero code on success and non-zero code on
    failure.

--- run_tests.sh
+++ run_tests.sh
@@ -20,55 +20,71 @@
 #
 ################################################################################
 
+# Args:
+#   $1: input file
+#   $2: expected stdout
+#   $3: expected stderr
+function run_one_test() {
+  local input="$1"
+  local expected_out="$2"
+  local expected_err="$3"
+
+  local filebase="$(basename "${input%.in}")"
+  local actual_out="$(mktemp "/tmp/${filebase}.out.XXXXXX")"
+  local actual_err="$(mktemp "/tmp/${filebase}.err.XXXXXX")"
+
+  # Run tests in a hermetic environment such that they don't break every year.
+  bash -c "./autogen.sh -y 2014 $(cat "${input}")" > "${actual_out}" 2> "${actual_err}"
+
+  local -i stdout=1
+  local -i stderr=1
+
+  if ! `diff "${expected_out}" "${actual_out}" > /dev/null 2>&1` ; then
+    stdout=0
+  fi
+
+  if ! `diff "${expected_err}" "${actual_err}" > /dev/null 2>&1` ; then
+    stderr=0
+  fi
+
+  local status_code=0
+  if [ -t 1 ]; then
+    local status="\033[0;32mPASSED\033[0m"
+  else
+    local status="PASSED"
+  fi
+
+  if [ ${stdout} -eq 0 ] || [ ${stderr} -eq 0 ]; then
+    status_code=1
+    if [ -t 1 ]; then
+      status="\033[0;31mFAILED\033[0m"
+    else
+      status="FAILED"
+    fi
+  fi
+
+  echo -e "${status} ${filebase}"
+
+  if [ ${stdout} -eq 0 ]; then
+    echo "  Differences in stdout; compare via: diff -u ${expected_out} ${actual_out}"
+  else
+    rm -f "${actual_out}"
+  fi
+
+  if [ ${stderr} -eq 0 ]; then
+    echo "  Differences in stderr; compare via: diff -u ${expected_err} ${actual_err}"
+  else
+    rm -f "${actual_err}"
+  fi
+
+  return ${status_code}
+}
+
 function run_all_tests() {
   for input in testdata/*.in; do
     local expected_out="${input%.in}.out"
     local expected_err="${input%.in}.err"
-
-    local filebase="$(basename "${input%.in}")"
-    local actual_out="$(mktemp "/tmp/${filebase}.out.XXXXXX")"
-    local actual_err="$(mktemp "/tmp/${filebase}.err.XXXXXX")"
-
-    # Run tests in a hermetic environment such that they don't break every year.
-    bash -c "./autogen.sh -y 2014 $(cat "${input}")" > "${actual_out}" 2> "${actual_err}"
-
-    local -i stdout=1
-    local -i stderr=1
-
-    if ! `diff "${expected_out}" "${actual_out}" > /dev/null 2>&1` ; then
-      stdout=0
-    fi
-
-    if ! `diff "${expected_err}" "${actual_err}" > /dev/null 2>&1` ; then
-      stderr=0
-    fi
-
-    if [ -t 1 ]; then
-      local status="\033[0;32mPASSED\033[0m"
-    else
-      local status="PASSED"
-    fi
-    if [ ${stdout} -eq 0 ] || [ ${stderr} -eq 0 ]; then
-      if [ -t 1 ]; then
-        status="\033[0;31mFAILED\033[0m"
-      else
-        status="FAILED"
-      fi
-    fi
-
-    echo -e "${status} ${filebase}"
-
-    if [ ${stdout} -eq 0 ]; then
-      echo "  Differences in stdout; compare via: diff -u ${expected_out} ${actual_out}"
-    else
-      rm -f "${actual_out}"
-    fi
-
-    if [ ${stderr} -eq 0 ]; then
-      echo "  Differences in stderr; compare via: diff -u ${expected_err} ${actual_err}"
-    else
-      rm -f "${actual_err}"
-    fi
+    run_one_test "${input}" "${expected_out}" "${expected_err}"
   done
 }
 
